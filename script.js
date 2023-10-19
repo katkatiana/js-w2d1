3dfc5c320342e7a12491b0a8b24e3baae399e929
Katia Santangelo <msantangelo56@gmail.com>
2023-10-19 18:43:40 +0200

    uploaded ex

---
+++ script.js
@@ -0,0 +1,148 @@
+//ESERCIZI SUGLI IF:
+
+/* ESERCIZIO 1
+ Scrivi un algoritmo per trovare il più grande tra due numeri interi.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let num1 = 12;
+let num2 = 33;
+if (num2 > num1) {
+  console.log(num2);
+} else {
+  console.log(num1)
+}
+
+/*
+ESERCIZIO 2
+  Crea un blocco condizionale if/else per mostrare in console il messaggio corretto in ogni condizione.
+
+  num < 5 - mostra in console "Tiny"
+  num < 10 - mostra in console "Small"
+  num < 15 - mostra in console "Medium"
+  num < 20 - mostra in console "Large"
+  num >= 20 - mostra in console "Huge"
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let num = 20;
+if (num < 5) {
+  console.log("Tiny");
+} else if (num < 10) {
+  console.log("Small");
+} else if (num < 15) {
+  console.log("Medium");
+} else if (num < 20) {
+  console.log("Large");
+} else if (num >= 20) {
+  console.log("Huge");
+} else {
+  console.log("Fail");
+}
+
+//ESERCIZI SUI CICLI:
+
+/* ESERCIZIO 3
+  Mostra i numeri da 0 a 10 (incluso) in ordine ascendente, ma evitando di mostrare i numeri 3 e 8 (suggerimento: ripassa l'uso di "continue").
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+for (let i = 0; i < 11; i++) {
+  if (i === 3) {
+    continue;
+  }
+  if (i === 8) {
+    continue;
+  }
+  console.log(i);
+}
+
+/* ESERCIZIO 11
+  Scrivi un ciclo in JavaScript per iterare da 0 a 15. Per ciascun elemento, il ciclo deve controllare the il valore corrente sia pari o dispari, e mostrare il risultato in console.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+for (let x = 0; x < 16; x++) {
+  if (x % 2 === 0) {
+    console.log(x, "Even");
+  } else {
+    console.log(x, "Odd");
+  }
+}
+
+//ESERCIZI EXTRA NON OBBLIGATORI
+
+/* ESERCIZIO EXTRA 1
+  Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let y = 8;
+let z = 10;
+if (y === 8 || z === 8) {
+  console.log("Yes, one of the numbers is eight");
+}
+
+if (y - z === 8) {
+  console.log("Well, their difference is eight");
+}
+
+/* ESERCIZIO EXTRA 2
+Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
+C'è una promozione in corso: se il totale del carrello supera 50, l'utente ha diritto alla spedizione gratuita (altrimenti la spedizione ha un costo fisso pari a 10).
+Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let firstItem = 20;
+let secondItem = 35;
+let deliveryCost = 10;
+let totalShoppingCart = firstItem + secondItem;
+let totalToPay;
+if (totalShoppingCart > 50) {
+  totalToPay = totalShoppingCart;
+} else {
+  totalToPay = totalShoppingCart + deliveryCost;
+}
+console.log("You have to pay " + totalToPay);
+
+/* ESERCIZIO EXTRA 3
+  Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
+  Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando, usando l'algoritmo del codice precedente, se le spedizioni siano gratuite oppure no e e calcolando il totale.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+totalShoppingCart =
+  firstItem - 0.2 * firstItem + (secondItem - 0.2 * secondItem);
+if (totalShoppingCart > 50) {
+  totalToPay = totalShoppingCart;
+} else {
+  totalToPay = totalShoppingCart + deliveryCost;
+}
+console.log("You have to pay " + totalToPay);
+/*  ESERCIZIO EXTRA 4
+  Usa un operatore ternaio per assegnare ad una variabile chiamata "gender" i valori "male" o "female".
+  La scelta deve essere basata sul valore di un'altra variabile booleana chiamata isMale.
+  Es. se isMale e' vero, il valore di gender deve essere "male"
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let isMale = true;
+let gender = isMale ? "male" : "female";
+/* OTHER SOLUTION : isMale ? gender ="male" : gender = "female" */
+console.log(gender);
+
+/* ESERCIZIO EXTRA 5
+  Scrivi un algoritmo che iteri i numeri da 1 a 100, stampandoli in console. Se un valore tuttavia è multiplo di 3 (operatore modulo!), stampa al suo posto la parola "Fizz" e se il numero è multiplo di 5, stampa "Buzz". Se le condizioni si verificano entrambe, stampa "FizzBuzz".
+*/
+for (let l = 1; l < 101; l++) {
+  if (l % 3 === 0 && l % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (l % 3 === 0) {
+    console.log("Fizz");
+  } else if (l % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(l);
+  }
+}
